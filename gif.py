f559cd079c55b1fe8f9be0f1892aaaa9e1566271
Olivier Georgeon <olivier.georgeon@gmail.com>
2026-02-21 12:47:53 +0100

    01_movie.mp4

--- gif.py
+++ gif.py
@@ -4,4 +4,4 @@ import os
 img_dir = f"./logs"
 all_files = [os.path.join(img_dir, f) for f in os.listdir(img_dir) if f.endswith('.png')]
 images = [imageio.imread(f) for f in all_files]
-imageio.mimsave("images/movie.gif", images, fps=5)
+imageio.mimsave("doc/img/01_movie.mp4", images, fps=5)
