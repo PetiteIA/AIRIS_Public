1a1fff9188ef2d5b23ef543db7a02bdc28d7e418
Olivier Georgeon <olivier.georgeon@gmail.com>
2026-03-26 15:14:30 +0100

    minor

--- gif.py
+++ gif.py
@@ -1,7 +1,7 @@
 import imageio.v2 as imageio
 import os
 
-img_dir = f"./logs"
+img_dir = f"./doc/sav"
 all_files = [os.path.join(img_dir, f) for f in os.listdir(img_dir) if f.endswith('.png')]
 images = [imageio.imread(f) for f in all_files]
-imageio.mimsave("doc/img/01_movie.mp4", images, fps=5)
+imageio.mimsave("doc/img/03_movie_3.gif", images)  #, fps=5)
